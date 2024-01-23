768472f56dd84ce3ee75b7a527ba92da2ac10b7e
Malahov-Alexandr <67895548+Malahov-Alexandr@users.noreply.github.com>
2024-01-23 23:59:40 +0200

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,11 @@
+@pytest.fixture
+def driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+    driver = webdriver.Chrome(options)
+    yield driver
+    driver.close()
+    driver.quit()
